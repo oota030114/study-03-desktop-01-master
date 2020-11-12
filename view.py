7bc805a5de99d427425c93dca4707d245ed86fec
oota030114 <74047634+oota030114@users.noreply.github.com>
2020-11-13 07:54:41 +0900

    Add files via upload

---
+++ view.py
@@ -0,0 +1,23 @@
+import eel
+import desktop
+import search
+
+app_name="html"
+end_point="index.html"
+size=(700,600)
+
+@ eel.expose
+def kimetsu_search(word):
+    search.kimetsu_search(word)
+
+# @ eel.expose
+# def alertMsg():
+#     print("検索キーワードを入力してください。")
+
+
+#HTML表示    
+desktop.start(app_name,end_point,size)
+#desktop.start(size=size,appName=app_name,endPoint=end_point)
+
+
+
