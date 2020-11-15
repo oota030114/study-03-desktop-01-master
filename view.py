a22c7afb2562a985b2fccb023a7e06006a7db0ad
oota030114 <74047634+oota030114@users.noreply.github.com>
2020-11-16 07:31:40 +0900

    Add files via upload

--- view.py
+++ view.py
@@ -1,6 +1,8 @@
 import eel
 import desktop
 import search
+import json
+from io import StringIO
 
 app_name="html"
 end_point="index.html"
@@ -9,9 +11,15 @@ size=(700,600)
 @ eel.expose
 def kimetsu_search(word):
     source = search.kimetsu_search(word)
-    eel.view_log_js(source)
+    print("source->")
+    print(source)
+    sourcejson=json.dumps(source)
+    print("sourcejson->")
+    print(sourcejson)
+    eel.view_log_js(sourcejson)
 
-#HTML表示    
+
+#HTML表示
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
 
