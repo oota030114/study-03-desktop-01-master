d7399457c658cde19990b288e7ce984a5ce0afa7
oota030114 <74047634+oota030114@users.noreply.github.com>
2020-12-02 20:30:18 +0900

    Add files via upload

--- search.py
+++ search.py
@@ -2,7 +2,7 @@ import pandas as pd
 import eel
 
 ### デスクトップアプリ作成課題
-def kimetsu_search(word):
+def kimetsu_search(word, saveFile):
     # 検索対象取得
     df=pd.read_csv("./source.csv")
     source=list(df["name"])
@@ -19,6 +19,6 @@ def kimetsu_search(word):
     
     # CSV書き込み
     df=pd.DataFrame(source,columns=["name"])
-    df.to_csv("./source.csv",encoding="utf_8-sig")
+    df.to_csv(savefolder,encoding="utf_8-sig")
 
     return source
